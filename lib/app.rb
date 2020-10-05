56fa7079d083ae0b926e9f8beebc24abcce1a85f
Garrett Brown <garrettpaulbrown@gmail.com>
2020-10-04 18:57:10 -0700

    Revert app.rb to previous state.

--- lib/app.rb
+++ lib/app.rb
@@ -7,8 +7,4 @@ puts 'Enter a word.'
 @word1 = gets 
 word1 = @word1
 puts 'Okay enter another word.'
-@word2 = gets
-word2 = @word2
 puts 'Your words are ' + @word1 + ' and ' + @word2 + '.'
-def anagrams (word1, word2)
-end
